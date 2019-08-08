173b93cf7c8228d5898ed417a0b8ac2cad456896
praveen7101994 <30315923+praveen7101994@users.noreply.github.com>
2019-08-08 17:30:30 +0530

    Updates

---
+++ precache-manifest.bd0eafb0bcd22f74c7c356d8a721d456.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "900249cf9dec1b047a4a",
+    "url": "/robofriends/static/js/main.0997cf72.chunk.js"
+  },
+  {
+    "revision": "98d555baddec17377c04",
+    "url": "/robofriends/static/js/2.c92d2834.chunk.js"
+  },
+  {
+    "revision": "900249cf9dec1b047a4a",
+    "url": "/robofriends/static/css/main.2ca2e220.chunk.css"
+  },
+  {
+    "revision": "98d555baddec17377c04",
+    "url": "/robofriends/static/css/2.2fbdca75.chunk.css"
+  },
+  {
+    "revision": "b4a2bceb09024d325682cec983e7dadd",
+    "url": "/robofriends/index.html"
+  }
+];
